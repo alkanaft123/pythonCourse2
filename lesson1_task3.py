edfe11d936dd49b06f4bb58ab1254a5642bc6d84
alkanaft <alkanaft123@yandex.ru>
2019-12-04 18:14:34 +0300

    lesson1
    lesson1

---
+++ lesson1_task3.py
@@ -0,0 +1,9 @@
+# task 3
+
+# По введенным пользователем координатам двух точек вывести уравнение прямой вида y = kx + b, проходящей через эти точки.
+
+x1 = int(input('enter x1'))
+y1 = int(input('enter y1'))
+x2 = int(input('enter x2'))
+y2 = int(input('enter y2'))
+print('y=',x2/x1,'x+(',y2-y1,')')
